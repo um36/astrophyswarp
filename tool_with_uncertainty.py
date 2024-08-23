631155479b14fed78fe3972e628f17bc08280b75
um36 <urvi11modha@gmail.com>
2024-08-23 17:00:11 +0100

    Update tool_with_uncertainty.py
    attempt 18

--- tool_with_uncertainty.py
+++ tool_with_uncertainty.py
@@ -20,7 +20,7 @@ from utils import (
     adjust_phase_interval,  
     plot_differences 
 )
-
+#check parameters of functions
 # Load the external CSS file
 with open('styles.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
